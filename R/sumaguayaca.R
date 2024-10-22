3c23a218560aaf36c660066fc9a9215e6c8ac840
gvacosta <gvacosta@espol.edu.ec>
2024-10-22 11:50:25 -0500

    Initial commit

---
+++ R/sumaguayaca.R
@@ -0,0 +1,16 @@
+#' Función suma guayaca
+#'
+#' @param a primer numero
+#' @param b segundo numero
+#' @param c tercer numero
+#' @param d cuarto numero
+#'
+#' @return el resultado de la suma guayaca
+#' @export
+#'
+#' @examples
+#' sumaguayaca(a = 1, b = 2, c = 3, d = 4)
+sumaguayaca <- function(a, b, c, d){
+  result <- a+b+c+d
+  return(result)
+}
